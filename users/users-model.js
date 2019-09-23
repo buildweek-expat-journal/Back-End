c1a606a4d15badb194bf3dd70ba82564f05c607e
Pat Palmerston <patpalmerston@gmail.com>
2019-09-23 10:37:24 -0700

    users model

---
+++ users/users-model.js
@@ -0,0 +1,31 @@
+const db = require('../data/dbConfig');
+
+module.exports = {
+	add,
+	find,
+	findById,
+	findBy
+};
+
+function add(user) {
+	return db('users')
+		.insert(user, 'id')
+		.then(ids => {
+			const [id] = ids;
+			return findById(id);
+		});
+}
+
+function find() {
+	return db('users').select('id', 'username');
+}
+
+function findBy(username) {
+	return db('users').where(username);
+}
+
+function findById() {
+	return db('users')
+		.where({ id })
+		.first();
+}
