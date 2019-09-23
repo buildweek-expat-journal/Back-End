2a4070fd312153138e4867b148d36f1175f65a9c
Pat Palmerston <patpalmerston@gmail.com>
2019-09-23 10:46:42 -0700

    photo model scaffolded

---
+++ photos/photos-model.js
@@ -0,0 +1,30 @@
+const db = require('../data/dbConfig')
+
+module.exports {
+  getPhotos,
+  getUserIdPhotos,
+  addPhoto,
+  findPhotoById,
+  removePhotoById,
+  updatePhoto
+}
+
+function getPhotos() {
+  return null
+}
+function getUserIdPhotos() {
+  return null
+}
+function addPhoto() {
+  return null
+}
+function findPhotoById() {
+  return null
+}
+function removePhotoById() {
+  return null
+}
+function updatePhoto() {
+  return null
+}
+
